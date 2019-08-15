c950577f4a29959cffad9b2a26c1b55748cdee3a
zoeyliang@tencent.com <zoeyliang@tencent.com>
2019-08-15 17:33:08 +0800

    test2

---
+++ Carthage/Build/iOS/new.framework/Headers/new.h
@@ -0,0 +1,19 @@
+//
+//  new.h
+//  new
+//
+//  Created by admin on 2019/8/15.
+//  Copyright © 2019年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for new.
+FOUNDATION_EXPORT double newVersionNumber;
+
+//! Project version string for new.
+FOUNDATION_EXPORT const unsigned char newVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <new/PublicHeader.h>
+
+
