4189eed99a2d47043940019e5d587878084ebdbe
zoeyliang@tencent.com <zoeyliang@tencent.com>
2019-08-16 09:39:17 +0800

    ios

---
+++ Carthage/Carthage.h
@@ -0,0 +1,19 @@
+//
+//  Carthage.h
+//  Carthage
+//
+//  Created by admin on 2019/8/15.
+//  Copyright © 2019年 XG of Tencent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Carthage.
+FOUNDATION_EXPORT double CarthageVersionNumber;
+
+//! Project version string for Carthage.
+FOUNDATION_EXPORT const unsigned char CarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Carthage/PublicHeader.h>
+
+
